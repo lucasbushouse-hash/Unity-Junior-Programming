62ef347083bcd276329f1352c40a05d38dc5926d
Luis <lucasbushouse@gmail.com>
2026-09-21 08:33:30 -0400

    1.4.6 Make the vehicle move forward and sideways

--- Create With Code/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Create With Code/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -1,21 +1,25 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
      public float speed = 20.0f;
-     public float turnSpeed = 20.0f;
+     public float turnSpeed;
+     public InputAction moveAction;
+     public Vector2 moveInput;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        moveAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
+        moveInput = moveAction.ReadValue<Vector2>();
+
         // We'll move the vehicle foward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        transfor.Translate(Vector3.right * Time.deltaTime * turnSpeed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * moveInput.y);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * moveInput.x);
     }
 }
