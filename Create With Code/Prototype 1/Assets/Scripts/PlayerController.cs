b170f596f946e4a1f2105de2a94ae040dcad37ad
Luis <lucasbushouse@gmail.com>
2026-09-22 07:46:24 -0400

    1.4.8  Clean my code and hierarchy

--- Create With Code/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Create With Code/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -2,25 +2,34 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
+    //Movement turning (editable in Inspector)
      public float speed = 20.0f;
      public float turnSpeed;
+     //Input System action exposed in INspector for binding (WASD / Arrow keys)
      public InputAction moveAction;
+
+     // Current input value (x = left/right, y = forward/back), kept private for internal use
      public Vector2 moveInput;
+     
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // Enable the MoveAction so it starts reading input
         moveAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Read the 2d vector from the MoveAction (x: horizontal, y: vertical)
         moveInput = moveAction.ReadValue<Vector2>();
 
-        // We'll move the vehicle foward
+        // Move forward/back along local Z using the y component
         transform.Translate(Vector3.forward * Time.deltaTime * speed * moveInput.y);
-        transform.Rotate(Vector.up, Time.deltaTime * turnSpeed * moveInput.x);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * moveInput.x);
+        
+        // Rotate around local Y (yaw) using the x component
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * moveInput.x);
+       
     }
 }
